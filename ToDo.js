362437ab9faa80c27929a8e900943935de2d3e97
Pushkar404 <pushkar8705@gmail.com>
2025-02-15 19:33:36 +0530

    Update ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -1,17 +1,19 @@
-let inputs = document.getElementById("inp");
-let text = document.querySelector(".text");
+let input = document.getElementById("inp");  // Fix variable name (singular "input" makes more sense)
+let text = document.querySelector(".text");  // Container for task list
 
-function Add(){
-    if(inputs.value == ""){
-        alert("Please Enter Task")
-    }else{
-        let newEle = document.createElement("ul");
-        newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
+function Add() {
+    if (input.value.trim() === "") {  // Trim to avoid spaces-only input
+        alert("Please enter a task");
+    } else {
+        let newEle = document.createElement("li");  // Use <li> instead of <ul>
+        newEle.innerHTML = `${input.value} <i class="fa-solid fa-trash"></i>`;
+
         text.appendChild(newEle);
-        inputs.value="";
-        newEle.querySelector("i").addEventListener("click" , remove);
-        function remove(){
-            newEle.remove()
-        }
+        input.value = ""; // Clear input field after adding task
+
+        // Add event listener for remove functionality
+        newEle.querySelector("i").addEventListener("click", function () {
+            newEle.remove();
+        });
     }
 }
